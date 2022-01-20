dfce0710416ed496ce49b45a83b5c4fd1f394e20
PavloYurchenko19 <stolbunpavlo@gmail.com>
2022-01-21 01:12:24 +0200

    n

---
+++ src/config/urls.js
@@ -0,0 +1,11 @@
+const baseURL = 'https://jsonplaceholder.typicode.com/';
+
+export default baseURL;
+
+export const urls = {
+    user: '/users',
+    posts: '/posts',
+    comments: '/comments',
+    alboms: '/albums',
+    foto: '/photos'
+}
