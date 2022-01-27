4944200a64a367eec51d2ae81414f778403495de
PavloYurchenko19 <stolbunpavlo@gmail.com>
2022-01-27 17:39:19 +0200

    Home work 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import './App.css';
+import {useReducer} from "react";
+import {useForm} from "react-hook-form";
+
+const reduce = (state,action) => {
+
+
+};
 
 function App() {
-  return (
+const [state,dispatch] = useReducer(reduce,{cat:'',dog:''})
+    let  {
+handleSubmit,
+        register
+    }
+        = useForm();
+
+    const submit = (e) => {
+        console.log(e.target.cat.value);
+
+    };
+
+    return (
+
       <div>
+          <div>
+              <form onSubmit={handleSubmit(submit)}>
+                  <label> cat<input type="text" defaultValue={''} {...register('cat')}/></label>
+                  <button> send</button>
+                  <label> dog<input type="text" defaultValue={''} {...register('dog')}/></label>
+                  <button> send</button>
+              </form>
+          </div>
 
       </div>
   );
