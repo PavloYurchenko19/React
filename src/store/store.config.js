cd3fec7663815f69af0a5a285c4a645fa3a4de1a
PavloYurchenko19 <stolbunpavlo@gmail.com>
2022-01-29 17:09:53 +0200

    Home wore 5

--- src/store/store.config.js
+++ src/store/store.config.js
@@ -1,8 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
+import usersReduce from "./users.slice";
 
 const store = configureStore({
     reducer:{
-
+        usersReduce
     }
 })
 export default store
--- src/store/users.slice.js
+++ src/store/users.slice.js
@@ -1,4 +1,18 @@
 import {createSlice,createAsyncThunk} from "@reduxjs/toolkit";
+import {usersService} from "../service/users.service";
+
+export const getAllUsers = createAsyncThunk(
+    'userSlice/getAllUsers',
+    async (_,{dispatch})=>{
+        try {
+            const users = await usersService.getAll()
+            dispatch(getAll({users}))
+
+        }catch (e){
+
+        }
+    }
+)
 
 const usersSlice = createSlice({
     name:'userSlice',
@@ -7,7 +21,13 @@ const usersSlice = createSlice({
     },
     reducers:{
         getAll:(state,action)=>{
-          state.users =
+          state.users.push(action.payload.users)
         },
+    },
+    extraReducers:{
+    []
     }
 })
+const usersReduce = usersSlice.reducer
+export const {getAll} = usersSlice.actions;
+export default usersReduce
